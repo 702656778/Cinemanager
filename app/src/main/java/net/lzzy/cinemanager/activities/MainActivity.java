9a828dcdd1bd810ed587604d6316b1c92d6464b9
YanZhenMin <702656778@qq.com>
2019-03-26 18:16:23 +0800

    2019年3月26日18:15:42

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,19 +1,85 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-
+import android.util.SparseArray;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrdersFragment;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private FragmentManager manager=getSupportFragmentManager();
+    private LinearLayout layoutMenu;
+    private TextView tvTitle;
+    private SearchView searchView;
+    private SparseArray<String> titleArray=new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+    }
+
+    private void setTitleMenu(){
+        titleArray.put(R.id.bar_title_tv_add_cinema,"");
+        layoutMenu=findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_iv_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int visible=layoutMenu.getVisibility()==View.VISIBLE?View.GONE:View.VISIBLE;
+                layoutMenu.setVisibility(visible);
+            }
+        });
+        tvTitle=findViewById(R.id.bar_title_tv);
+        tvTitle.setText(R.string.bar_title_tv_my_order);
+        searchView=findViewById(R.id.bar_title_sv);
+        findViewById(R.id.bar_title_tv_my_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        layoutMenu.setVisibility(View.GONE);
+        switch (v.getId()){
+            case R.id.bar_title_tv_add_cinema:
+                break;
+            case R.id.bar_title_tv_view_cinema:
+                tvTitle.setText("影院列表");
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container, new CinemasFragment())
+                        .commit();
+                break;
+            case R.id.bar_title_tv_add_order:
+                break;
+            case R.id.bar_title_tv_my_order:
+                tvTitle.setText("我的订单");
+                manager.beginTransaction()
+                        .replace(R.id.fragment_container,new OrdersFragment())
+                        .commit();
+                break;
+                default:
+                    break;
+        }
     }
 }
